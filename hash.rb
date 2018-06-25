8182578e02059fa98c5fc42e46d8a2655a855a7f
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-06-25 22:34:15 +0300

    Add hash

---
+++ hash.rb
@@ -0,0 +1,54 @@
+matz = { "First name" => "Yukihiro",
+         "Last name" => "Matsumoto",
+         "Age" => 47,
+         "Nationality" => "Japanese",
+         "Nickname" => "Matz"
+}
+
+matz.each do |key, value|
+  puts matz[key]
+end
+
+# Converting Between Symbols and Strings
+strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+symbols = []
+
+strings.each do |s|
+  symbols.push(s.to_sym) # symbols.push(s.intern)
+end
+print symbols
+
+#HASHES AND SYMBOLS
+
+require 'benchmark'
+
+string_AZ = Hash[("a".."z").to_a.zip((1..26).to_a)]
+symbol_AZ = Hash[(:a..:z).to_a.zip((1..26).to_a)]
+
+string_time = Benchmark.realtime do
+  100_000.times { string_AZ["r"] }
+end
+
+symbol_time = Benchmark.realtime do
+  100_000.times { symbol_AZ[:r] }
+end
+
+puts "String time: #{string_time} seconds."
+puts "Symbol time: #{symbol_time} seconds."
+
+# Becoming More Selective
+movie_ratings = {
+    memento: 3,
+    primer: 3.5,
+    the_matrix: 5,
+    truman_show: 4,
+    red_dawn: 1.5,
+    skyfall: 4,
+    alex_cross: 2,
+    uhf: 1,
+    lion_king: 3.5
+}
+
+good_movies = movie_ratings.select { |name, rating| rating > 3 }
+
+print good_movies
